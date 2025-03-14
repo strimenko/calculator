b5605a9d32da13e4211ba849ee06f08b1ac5ea30
strimenko <zhiharev.danil00@gmail.com>
2025-03-14 13:58:43 +0300

    second commit

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
+	var a float64
+	var b float64
+	var input string
+	var err error
 	for {
-		var a float32
-		var b float32
-		var value string
-		fmt.Print("Введите первое число:")
-		fmt.Scan(&a)
-		fmt.Print("Введите второе число:")
-		fmt.Scan(&b)
-		fmt.Print("Введите математическую операцию ( + , - , * , / ) :")
-		fmt.Scan(&value)
+		for {
+			fmt.Print("Введите первое число:")
+			fmt.Scan(&input)
+			a, err = isValidNumber(input)
+			if err == nil {
+				break
+			} else {
+				fmt.Println("Ошибка:", err)
+			}
+		}
 
-		switch value {
+		for {
+			fmt.Print("Введите второе число:")
+			fmt.Scan(&input)
+			b, err = isValidNumber(input)
+			if err == nil {
+				break
+			} else {
+				fmt.Println("Ошибка:", err)
+			}
+		}
+
+		for {
+			fmt.Println("Введите математический знак (+, -, *, /):")
+			fmt.Scanln(&input)
+
+			if isValidOperator(input) {
+				break
+			}
+			fmt.Println("Ошибка: введен неверный знак. Пожалуйста, используйте +, -, * или /.")
+		}
+
+		switch input {
 		case "+":
 			fmt.Println(a + b)
 		case "-":
@@ -23,9 +53,30 @@ func main() {
 			fmt.Println(a * b)
 		case "/":
 			fmt.Println(a / b)
-		default:
-			fmt.Println("unknown literal")
 		}
 
 	}
+}
+
+func isValidNumber(input string) (float64, error) {
+	input = strings.TrimSpace(input)
+
+	num, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, fmt.Errorf("неверное число: %s", input)
+	}
+
+	return num, nil
+}
+
+func isValidOperator(input string) bool {
+	validOperators := []string{"+", "-", "*", "/"}
+
+	input = strings.TrimSpace(input)
+	for _, op := range validOperators {
+		if input == op {
+			return true
+		}
+	}
+	return false
 }
